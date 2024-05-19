bde29cf5e6cb2f29bc3eded8fe324d505cde70a3
Andy Brown <andybrown7890@gmail.com>
2024-05-19 21:53:16 +0100

    building

--- dist/handler.js
+++ dist/handler.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getLoot = exports.hello = void 0;
+const data_1 = require("./consts/data");
 const hello = async (event) => {
     return {
         statusCode: 200,
@@ -15,7 +16,7 @@ const getLoot = async (event) => {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'You got the loot!',
+            message: data_1.ITEMS,
             input: event,
         }),
     };
