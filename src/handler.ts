8f03c5dcbf085e212c33ec051e0da3ebed95337c
Andy Brown <andybrown7890@gmail.com>
2024-05-20 13:52:37 +0100

    CORS workaround

--- src/handler.ts
+++ src/handler.ts
@@ -14,6 +14,10 @@ export const hello: APIGatewayProxyHandler = async (event) => {
 export const getLoot: APIGatewayProxyHandler = async (event) => {
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+      },
       body: JSON.stringify({
         message: ITEMS,
         input: event,
