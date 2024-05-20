3b64e602a45918e896571d7fa6f119fa536c4c65
Andy Brown <andybrown7890@gmail.com>
2024-05-20 14:50:38 +0100

    build artefact

--- dist/handler.js
+++ dist/handler.js
@@ -15,6 +15,12 @@ exports.hello = hello;
 const getLoot = async (event) => {
     return {
         statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*', // Allow any origin, or specify a particular origin
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS', // Allowed methods
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Allowed headers
+        },
         body: JSON.stringify({
             message: data_1.ITEMS,
             input: event,
