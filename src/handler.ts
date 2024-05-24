57b19dc4599fd412ff880168b27f7f77bed65ba8
Andy Brown <andybrown7890@gmail.com>
2024-05-24 18:44:40 +0100

    Prothero Support

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,22 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler, APIGatewayProxyEvent } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
 import { ITEMS } from './consts/data';
 
-export const hello: APIGatewayProxyHandler = async (event) => {
+// Initialize DynamoDB client
+const dynamoDb = new DynamoDB.DocumentClient();
+const PLAYERS_TABLE_NAME = 'prothero_db_players';
+
+type EventError = any;
+
+const headers = {
+  'Content-Type': 'application/json',
+  // CORS Fixes
+  'Access-Control-Allow-Origin': '*', 
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
+
+export const hello: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -11,18 +26,83 @@ export const hello: APIGatewayProxyHandler = async (event) => {
   };
 };
 
-export const getLoot: APIGatewayProxyHandler = async (event) => {
+export const getLoot: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) => {
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify({
+      message: ITEMS,
+      input: event,
+    }),
+  };
+};
+
+export const getProthero: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) => {
+  const params = {
+    TableName: PLAYERS_TABLE_NAME,
+  };
+
+  try {
+    const data = await dynamoDb.scan(params).promise();
     return {
       statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*', // Allow any origin, or specify a particular origin
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS', // Allowed methods
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Allowed headers
-      },
+      headers,
       body: JSON.stringify({
-        message: ITEMS,
+        message: data.Items,
         input: event,
       }),
     };
+  } catch (error: EventError) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Error fetching data',
+        error: error.message,
+      }),
+    };
+  }
+};
+
+export const setProthero: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) => {
+
+  if (event.body === null) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        message: 'Invalid request body',
+      }),
+    };
+  }
+
+  const { id, name, type } = JSON.parse(event.body);
+
+  const params = {
+    TableName: PLAYERS_TABLE_NAME,
+    Item: {
+      id,
+      name,
+      type,
+    },
   };
+
+  try {
+    await dynamoDb.put(params).promise();
+    return {
+      statusCode: 200,
+      
+      body: JSON.stringify({
+        message: 'Item added',
+        input: event,
+      }),
+    };
+  } catch (error: EventError) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Error adding item',
+        error: error.message,
+      }),
+    };
+  }
+};
